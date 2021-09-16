b95eab6fc143f62ae6c5d50ffae31ecc4e6b057e
BEAUTAR <83558631+BEAUTAR@users.noreply.github.com>
2021-09-16 22:12:19 +0300

    landing page

---
+++ src/routes/styles.svelte
@@ -0,0 +1,58 @@
+
+<style>
+    .welMes{
+        display : flex;
+        flex-direction: row;
+        justify-content: flex-start;   
+    }
+    .welMes{
+        margin-left :3rem;
+        position: absolute;
+        width: 500px;
+        height: 28px;
+        margin-left: 5rem;
+        top: 205px;
+
+        font-family: Google Sans;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 10px;
+        line-height: 28px;
+    
+
+        letter-spacing: 0.06em;
+        text-transform: uppercase;
+
+        color:#444444;
+    }
+    .lkami{
+        position: absolute;
+        width: 499px;
+        height: 203px;
+        left: 86px;
+        top: 140px;
+
+        font-family: Abhaya Libre ExtraBold;
+        font-style: normal;
+        font-weight: 800;
+        font-size: 95px;
+        line-height: 203px;
+        letter-spacing: -0.04em;
+        color: #CB3322;
+    }
+    .lots{
+        position: absolute;
+        width: 570px;
+        height: 90px;
+        left: 94px;
+        top: 490px;
+
+        font-family: Google Sans;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 15px;
+        line-height: 30px;
+        color: #505050;
+
+    }
+</style>
